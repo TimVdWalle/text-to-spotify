266c16b63a80114727b4fd0dc3f34a646c704ddb
timvandewalle <tim@vandewalle.mobi>
2023-02-03 22:28:48 +0100

    Initial commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class HomeController extends Controller
+{
+    public function home(){
+        return view('home');
+    }
+    public function store(){
+        dd('ok');
+    }
+}
