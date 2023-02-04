09b52f97338b082955942fa4a7c818bdf9f47635
timvandewalle <tim@vandewalle.mobi>
2023-02-04 13:34:16 +0100

    refactore into multiple versions

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,51 +14,6 @@ class HomeController extends Controller
 {
     public function home()
     {
-        return view('home');
-    }
-
-    public function store(Request $request)
-    {
-        $text = $request->get('text');
-
-        if (!$text) {
-            return redirect(route('home'));
-        }
-
-        if (strlen($text) > 100) {
-            return redirect(route('home'));
-        }
-
-        $parts = explode(" ", $text);
-        if (count($parts) > 10) {
-            return redirect(route('home'));
-        }
-
-        $spotify = new Spotify(config('spotify.default_config'));
-
-        $results = collect();
-        foreach ($parts as $part) {
-            $searchResults = $spotify
-                ->searchTracks($part)
-                ->limit(10)
-                ->offset(0)
-                ->get();
-
-            if(isset($searchResults['tracks']) && isset($searchResults['tracks']['items'])){
-                foreach($searchResults['tracks']['items'] as $searchResult){
-
-                    if(strcasecmp($searchResult['name'], $part)){
-                        $result = [$part => $searchResult['id']];
-
-                        $results->push($result);
-                        break;
-                    }
-                }
-            }
-
-        }
-        dd($results);
-
-
+        return redirect(route('v1.show'));
     }
 }
