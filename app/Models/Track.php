e7893ebab0f66b175334c1d3498382df9cb6a8c6
timvandewalle <tim@vandewalle.mobi>
2023-02-18 15:03:46 +0100

    ✨ Add track model + db migration

---
+++ app/Models/Track.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property string $track_name
+ */
+
+class Track extends Model
+{
+    use HasFactory;
+
+    /**
+     * @property string $track_name
+     */
+    protected $fillable = [
+        'track_name',
+        'track'
+    ];
+
+    protected $casts = [
+        'track' => 'object'
+    ];
+
+    protected function track(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => json_decode($value, true),
+            set: fn ($value) => json_encode($value),
+        );
+    }
+}
